527e6cc101b21524e23cd0ec8a85036de51f1aa1
Tomas Barton <barton.tomas@gmail.com>
2016-04-28 00:49:05 +0200

    update install instructions for ubuntu

--- install.R
+++ install.R
@@ -1,8 +1,8 @@
 packages <- function(pkg){
       new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
     if (length(new.pkg))
-              install.packages(new.pkg, dependencies = TRUE, repos='http://cran.rstudio.com/', lib="~/R/library")
+              install.packages(new.pkg, dependencies = TRUE, repos='http://cran.rstudio.com/')
         sapply(pkg, require, character.only = TRUE)
 }
 
-packages(c("Rserve","cluster","fields","kernlab","lattice","MASS","mlbench","stats","proxy","fpc","clv","lars","kohonen","clusterGeneration"))
+packages(c("Rserve","cluster","tables","fields","igraph","kernlab","lattice","MASS","mlbench","stats","proxy","fpc","clv","lars","kohonen","clusterGeneration","tnet"))
